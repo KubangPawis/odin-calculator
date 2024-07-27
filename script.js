ea0769051af8e223bcb620a7a89e944884b17277
KubangPawis <xanderfernandezperegrin@gmail.com>
2024-07-27 19:47:13 +0800

    feat: add operate(result getter) function

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ function multiply(firstNum, secondNum) {
 function divide(firstNum, secondNum) {
     return firstNum / secondNum;
 }
+
+function operate(firstNum, secondNum, operator) {
+    let result;
+    switch (operator) {
+        case "+":
+            result = add(firstNum, secondNum);
+            break;
+        case "-":
+            result = subtract(firstNum, secondNum);
+            break;
+        case "×":
+            result = multiply(firstNum, secondNum);
+            break;
+        case "÷":
+            result = divide(firstNum, secondNum);
+            break;
+    }
+    return result;
+}
+
+let firstNum;
+let secondNum;
+let operator;
